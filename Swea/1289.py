56641f0de154289be851d650d5412078d71fd76d
MINJU <alswn9938@gmail.com>
2023-05-20 23:43:53 +0900

    add(Swea) : 1289

---
+++ Swea/1289.py
@@ -0,0 +1,15 @@
+T = int(input())
+
+for t in range(1, T+1) :
+    memory = list(input())
+
+    num = 0
+    reset = ['0'] * len(memory)
+    for i in range(len(memory)) :
+        if memory[i] != reset[i] :
+            for j in range(i, len(memory)) :
+                reset[j] = memory[i]
+            num += 1
+
+    print("#{} {}".format(t, num))
+
